f518c942673d4f3ce5399d66f06313e26f6d8232
firman1616 <49678848+firman1616@users.noreply.github.com>
2019-05-29 09:13:27 +0700

    Add files via upload

---
+++ Histogram.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+import scipy.stats
+
+# example data
+mu = 100 # mean of distribution
+sigma = 15 # standard deviation of distribution
+x = mu + sigma * np.random.randn(10000)
+
+num_bins = 20
+# the histogram of the data
+n, bins, patches = plt.hist(x, num_bins, density=1, facecolor='blue', alpha=0.5)
+
+# add a 'best fit' line
+y = scipy.stats.norm.pdf(bins, mu, sigma)
+plt.plot(bins, y, 'r--')
+plt.xlabel('Smarts')
+plt.ylabel('Probability')
+plt.title(r'Histogram of IQ: $\mu=100$, $\sigma=15$')
+
+# Tweak spacing to prevent clipping of ylabel
+plt.subplots_adjust(left=0.15)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
